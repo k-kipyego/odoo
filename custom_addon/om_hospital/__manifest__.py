34a9d49ff32fac26671edd4ef106708b0d3baa2e
Victor Kipyego <victor.kipyego@technobraingroup.com>
2024-11-28 17:24:04 +0300

    update

--- custom_addon/om_hospital/__manifest__.py
+++ custom_addon/om_hospital/__manifest__.py
@@ -8,6 +8,8 @@
     "data": [
         "security/ir.model.access.csv",
         "views/patient_views.xml",
+        "data/sequence.xml",
+        "views/appointment_views.xml",
         "views/patient_readonly_views.xml",
         "views/menu.xml",
     ]
